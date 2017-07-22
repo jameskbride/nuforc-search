76463b6d2c94c46e19eac7efb8382528ecf5f5b7
Jim Kirkbride <james.kirkbride@gmail.com>
2017-07-22 16:45:44 -0400

    Fighting the reverse proxy... Can now hit elasticsearch, need to figure out path.

--- web/nuforc-web/src/app/nuforc-search.service.ts
+++ web/nuforc-web/src/app/nuforc-search.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { NuforcEncounter } from './nuforc-encounter'
+import {Http} from "@angular/http";
 
 @Injectable()
 export class NuforcSearchService {
 
-  constructor() { }
+
+  constructor(private http: Http) { }
 
   search(queryString: String): Promise<NuforcEncounter[]>{
-    return Promise.resolve([
-      new NuforcEncounter('stuff', 'summary'),
-      new NuforcEncounter('more stuff', 'other summary')
-    ]);
+    const searchUrl = '/api/encounters/_search?q=';
+    let query = `${searchUrl}${queryString}`;
+    return this.http.get(query)
+      .toPromise()
+      .then(response => response.json().data as NuforcEncounter[]).catch(this.handleError);
+
+    // return Promise.resolve([
+    //   new NuforcEncounter('stuff', 'summary'),
+    //   new NuforcEncounter('more stuff', 'other summary')
+    // ]);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
 }
