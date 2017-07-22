04a6926c684ef96642b03de66444032f9202b97f
Jim Kirkbride <james.kirkbride@gmail.com>
2017-07-22 19:57:38 -0400

    Updated elasticsearch scripts to pass the base url.

--- elasticsearch/load-elasticsearch.sh
+++ elasticsearch/load-elasticsearch.sh
@@ -2,5 +2,5 @@
 
 for file in $1/*.json
 do
-    curl -X POST -H "Content-Type: application/json"  -T "$file" http://localhost:8080/encounters/encounter;
+    curl -X POST -H "Content-Type: application/json"  -T "$file" $2/encounters/encounter;
 done;
