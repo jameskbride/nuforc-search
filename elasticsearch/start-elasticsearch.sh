64674a056676aaa204ae5122f9e5fa683f5c18f6
Jim Kirkbride <james.kirkbride@gmail.com>
2017-10-13 19:28:57 -0400

    Removing nginx because angular4 isn't building prod correctly.

--- elasticsearch/start-elasticsearch.sh
+++ elasticsearch/start-elasticsearch.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker run -d -v "$PWD/esdata":/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 nuforc-elasticsearch
-
-
+docker run -d -v "$PWD/esdata":/usr/share/elasticsearch/data -v "$PWD/config":/usr/share/elasticsearch/config -p 9200:9200 -p 9300:9300 elasticsearch
--- web/nuforc-web/src/environments/environment.prod.ts
+++ web/nuforc-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: "http://localhost/api"
+  BASE_URL: "http://localhost:9200/api"
 };
